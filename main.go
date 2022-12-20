e17ff88d9458d852f54244cb13cb02b895f384a4
OneeePunchMan <83185557+OneeePunchMan@users.noreply.github.com>
2022-12-20 16:21:33 +0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func main() {
+	//создание бота с указанием токена
+	bot, err := tgbotapi.NewBotAPI("5783745041:AAEp87HA5TBy5TqXAIVO0IcCAVlkpSw9t50")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+
+		}
+	}
+}
