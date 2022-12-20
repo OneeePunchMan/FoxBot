3bbe33b643896928e2533cef55d5dcf4c4943f7a
OneeePunchMan <83185557+OneeePunchMan@users.noreply.github.com>
2022-12-20 16:28:15 +0400

    delete reply

--- main.go
+++ main.go
@@ -26,11 +26,6 @@ func main() {
 		if update.Message != nil { // If we got a message
 			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			bot.Send(msg)
-
 		}
 	}
 }
